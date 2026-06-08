64ed7365438061de19ee94e0b1fbd69570b3a799
smilefinancesolution01-coder <smilefinancesolution01@gmail.com>
2026-06-08 16:29:21 +0530

    Add Firestore initialization to firebase config

--- firebase/config.js
+++ firebase/config.js
@@ -1,5 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBnYNivxRjGww5McuQK0LI1wyjIJp-RxjQ",
@@ -7,10 +10,14 @@ const firebaseConfig = {
   projectId: "smile-financial-solution",
   storageBucket: "smile-financial-solution.firebasestorage.app",
   messagingSenderId: "681685112372",
-  appId: "1:681685112372:web:b52f13416ed550ba3b5c61"
+  appId: "1:681685112372:web:b52f13416ed550ba3b5c61",
+  measurementId: "G-3515WX2KHJ"
 };
 
 const app = initializeApp(firebaseConfig);
+
 const auth = getAuth(app);
 
-export { auth };
+const db = getFirestore(app);
+
+export { auth, db };
